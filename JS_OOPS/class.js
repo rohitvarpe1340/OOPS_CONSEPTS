631ea8ffb9e13cc15f53355cd2710501c1c33edf
Rohit Varpe <rohitvarpe1340@gmai.com>
2026-03-25 10:25:02 +0530

    add proper class and obj

--- JS_OOPS/class.js
+++ JS_OOPS/class.js
@@ -1,1 +1,15 @@
-let a =10;
+class Student{
+    constructor(name , age , education){
+        this.name =name;
+        this.age =age ;
+        this.education = education;
+
+    }
+    display(){
+        console.log(`student name :${this.name}`);
+        console.log(`student age : ${this.age}`);
+        console.log(`student education : ${this.education}`);
+    }
+}
+let  s1 = new  Student ("rohit",22,"MCA");
+s1.display();
